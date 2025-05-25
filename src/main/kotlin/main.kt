d6d8d7d49bf61645ef6f9f0ed8f2fd70d4e9e850
Мартынова Екатерина Михайловна <ekaterina.martynova89@mail.ru>
2025-05-25 17:32:48 +0300

    minor edit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,4 @@
 fun main() {
-    val likes: Int = 10521
-    val result = if ((likes % 10) === 1 ) println("$likes человеку") else println("$likes людям")
+    val likes: Int = 101
+    val result = if ((likes % 10) === 1 && (likes % 100) != 11) println("Понравилось $likes человеку") else println("Понравилось $likes людям")
 }
